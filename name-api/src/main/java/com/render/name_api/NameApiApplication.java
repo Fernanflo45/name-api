d4c2ddd4e781ff77c38c6809b4d1f990afd6e2a5
Fernanflo45 <141339645+Fernanflo45@users.noreply.github.com>
2024-08-18 15:46:41 -0600

    Add files via upload

---
+++ name-api/src/main/java/com/render/name_api/NameApiApplication.java
@@ -0,0 +1,13 @@
+package com.render.name_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameApiApplication.class, args);
+	}
+
+}
---
+++ name-api/src/test/java/com/render/name_api/NameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.render.name_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
